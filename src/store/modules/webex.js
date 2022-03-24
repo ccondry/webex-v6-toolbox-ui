37b44b2af6ee368aa8039a7b512b3751465cb35e
Coty Condry <ccondry@cisco.com>
2022-03-24 18:18:59 -0500

    fix issue where joining the support space always showed success

--- src/store/modules/webex.js
+++ src/store/modules/webex.js
@@ -8,36 +8,36 @@ const getters = {
 
 const actions = {
   async joinSupportRoom ({dispatch, getters}, email) {
-    try {
-      await dispatch('fetch', {
-        group: 'webex',
-        type: 'joinSupportRoom',
-        url: getters.endpoints.webex,
-        options: {
-          method: 'POST',
-          body: {personEmail: email}
-        },
-        showNotification: false,
-        message: 'join Webex support room'
-      })
-      Toast.open({
-        type: 'is-success',
-        message: `You have been added to the support space`
-      })
-    } catch (e) {
-      if (e.status === 409) {
+    const response = await dispatch('fetch', {
+      group: 'webex',
+      type: 'joinSupportRoom',
+      url: getters.endpoints.webex,
+      options: {
+        method: 'POST',
+        body: {personEmail: email}
+      },
+      showNotification: false,
+      message: 'join Webex support room'
+    })
+    if (response instanceof Error) {
+      if (response.status === 409) {
         Toast.open({
           type: 'is-success',
-          message: `You have been added to the support space`
+          message: `You are already in the support space.`
         })
       } else {
         Toast.open({
-          message: `Failed to add you to the support space: ${e.message}`,
+          message: `Failed to add you to the support space: ${response.message}`,
           type: 'is-danger',
           duration: 6 * 1000,
           queue: false
         })
       }
+    } else {
+      Toast.open({
+        type: 'is-success',
+        message: `You have been added to the support space.`
+      })
     }
   }
 }

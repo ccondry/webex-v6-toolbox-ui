14cb1f44374a5fa35969e807451da21d880d8918
Coty Condry <ccondry@cisco.com>
2022-04-21 09:46:53 -0500

    remove time estimate from the provision in progress message

--- src/components/provision-progress.vue
+++ src/components/provision-progress.vue
@@ -1,8 +1,7 @@
 <template>
   <panel title="Provision In Progress" aria-id="provision-in-progress">
     <p>
-      Your account is currently being provisioned. Please allow 
-      10-15 minutes for your account to be ready.
+      Your account is currently being provisioned.
     </p>
     <p>
       When your account is ready we will notify you with a Webex message, and

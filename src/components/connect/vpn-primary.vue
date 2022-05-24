546ae8fa516e2681c4008da42c72bdc4a45443c2
Coty Condry <ccondry@cisco.com>
2022-05-24 14:27:45 -0500

    add missing getter for 'working'

--- src/components/connect/vpn-primary.vue
+++ src/components/connect/vpn-primary.vue
@@ -59,7 +59,8 @@ export default {
     ...mapGetters([
       'vpnAddress',
       'userDemoConfig',
-      'instance'
+      'instance',
+      'working'
     ]),
     vpnUsername () {
       try {

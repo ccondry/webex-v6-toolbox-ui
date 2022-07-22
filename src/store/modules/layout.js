b00018a128e5d5ffbbaf946c24e06ed1a9c906f8
geekdcloud <atorresc@cisco.com>
2022-07-22 12:36:50 -0600

    background fix

--- src/store/modules/layout.js
+++ src/store/modules/layout.js
@@ -126,7 +126,7 @@ const actions = {
       // create a copy of the layout
       const layoutCopy = JSON.parse(JSON.stringify(currentLayout))
       // set the logo image URL
-      layoutCopy.taskPageIllustration = response.url.url
+      layoutCopy.landingPageIllustration = response.url.url
       // update user layout
       await dispatch('updateDesktopLayout', layoutCopy)
       // dispatch('getDesktopLayout')
